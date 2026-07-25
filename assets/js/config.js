12f8124202aabe5cddf0d8b4a017187bac84f043
Pakseh <dasbyu6@gmail.com>
2026-07-25 23:38:17 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,1 @@
-// =============================
-// KONFIGURASI FRONTEND
-// =============================
-
 const API_URL = "https://script.google.com/macros/s/AKfycbzcrFkXGJzV8WHD-1vKQaufBtrhPhMHMEFKYwjyQk8fPKKis8nDzxCeOh1M5JwpO4_Q/exec";
