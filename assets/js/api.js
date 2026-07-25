bebb52b6ce3dbe4a65d0e9f31ff98032cf5fcee1
Pakseh <dasbyu6@gmail.com>
2026-07-25 23:10:07 +0700

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,45 @@
+// ============================
+// SLOT API
+// ============================
 
+async function registerUser(username, email, password) {
+
+    try {
+
+        const response = await fetch(API_URL, {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+
+                action: "register",
+
+                username: username,
+
+                email: email,
+
+                password: password
+
+            })
+
+        });
+
+        return await response.json();
+
+    } catch (err) {
+
+        return {
+
+            success: false,
+
+            message: err.message
+
+        };
+
+    }
+
+}
