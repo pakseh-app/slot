0e6d8382d1e0b4ff6cda5c9f729568fa1bbf3294
Pakseh <dasbyu6@gmail.com>
2026-07-25 23:08:47 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,1 @@
-
+const API_URL = "https://script.google.com/macros/s/AKfycbzcrFkXGJzV8WHD-1vKQaufBtrhPhMHMEFKYwjyQk8fPKKis8nDzxCeOh1M5JwpO4_Q/exec";const API_URL = "https://script.google.com/macros/s/AKfycbzcrFkXGJzV8WHD-1vKQaufBtrhPhMHMEFKYwjyQk8fPKKis8nDzxCeOh1M5JwpO4_Q/exec";
